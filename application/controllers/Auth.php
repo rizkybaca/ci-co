6c29ad06d9a328c547a219c562cd74d40a1184c7
ta <akun.rizky.baca@gmail.com>
2021-07-27 04:33:53 +0700

    pre auth

---
+++ application/controllers/Auth.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->library('form_validation');
+  }
+
+  public function index()
+  {
+
+    $this->load->view('templates/auth_header');
+    $this->load->view('auth/login');
+    $this->load->view('templates/auth_footer');
+  }
+
+  public function regist()
+  {
+    $this->load->view('templates/auth_header');
+    $this->load->view('auth/regist');
+    $this->load->view('templates/auth_footer');
+  }
+}
