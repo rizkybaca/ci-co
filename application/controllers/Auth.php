cb351f681a99da21ac1eacdfb14e9d26afc5bc6e
ta <akun.rizky.baca@gmail.com>
2021-07-27 14:49:36 +0700

    login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -12,28 +12,61 @@ class Auth extends CI_Controller
 
   public function index()
   {
-    $data['title'] = 'CO Login Page';
-    $this->load->view('templates/auth_header', $data);
-    $this->load->view('auth/login');
-    $this->load->view('templates/auth_footer');
+    $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+    $this->form_validation->set_rules('password', 'Password', 'required|trim');
+    if ($this->form_validation->run() == FALSE) {
+      $data['title'] = 'CO Login Page';
+      $this->load->view('templates/auth_header', $data);
+      $this->load->view('auth/login');
+      $this->load->view('templates/auth_footer');
+    } else {
+      $this->_login();
+    }
+  }
+  private function _login()
+  {
+    $email = $this->input->post('email');
+    $password = $this->input->post('password');
+
+    $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+    if ($user) {
+      if ($user['is_active'] == 1) {
+        if (password_verify($password, $user['password'])) {
+          $data = [
+            'email' => $user['email'],
+            'role_id' => $user['role_id']
+          ];
+          $this->session->set_userdata($data);
+          redirect('user/index');
+        } else {
+          $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong password.</div>');
+          redirect('auth');
+        }
+      } else {
+        $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This email has not been activated.</div>');
+        redirect('auth');
+      }
+    } else {
+      $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email is not registered.</div>');
+      redirect('auth');
+    }
   }
 
   public function regist()
   {
     $this->form_validation->set_rules('name', 'Name', 'required|trim');
-    $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
-    // $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
-    //   'is_unique' => 'Email has already registered!'
-    // ]);
+    $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+      'is_unique' => 'Email has already registered!'
+    ]);
     $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[4]|matches[password2]', [
       'matches' => 'Password dont match!',
       'min_length' => 'Password too short!'
     ]);
     $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
-    $this->form_validation->set_rules('phone', 'Phone Number', 'required|trim|numeric');
-    // $this->form_validation->set_rules('phone', 'Phone number', 'required|trim|numeric|is_unique[user.phone]', [
-    //   'is_unique' => 'Phone number has already registered!'
-    // ]);
+    $this->form_validation->set_rules('phone', 'Phone number', 'required|trim|numeric|is_unique[user.phone]', [
+      'is_unique' => 'Phone number has already registered!'
+    ]);
 
     if ($this->form_validation->run() == FALSE) {
       $data['title'] = 'CO Registration Page';
@@ -47,7 +80,7 @@ class Auth extends CI_Controller
         'password' => htmlspecialchars(password_hash($this->input->post('password1', true), PASSWORD_DEFAULT)),
         'phone' => htmlspecialchars($this->input->post('phone', true)),
         'image' => 'default.jpg',
-        'role_id' => 2,
+        'role_id' => 4,
         'is_active' => 1,
         'date_created' => time(),
         'date_modified' => time()
@@ -56,5 +89,13 @@ class Auth extends CI_Controller
       $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! Your account has been created. Please login</div>');
       redirect('auth');
     }
+  }
+  public function logout()
+  {
+    $this->session->unset_userdata('email');
+    $this->session->unset_userdata('role_id');
+
+    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">You have been logged out</div>');
+    redirect('auth');
   }
 }
